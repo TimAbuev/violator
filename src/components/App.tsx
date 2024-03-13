95a31669de055d80e4f7692ed15be837eb116220
timabuev <tim-r10@yandex.ru>
2024-03-13 14:20:12 +0300

    feat: add parameter to mixin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,12 +11,18 @@ function App() {
           aperiam libero voluptatem hic quod vero optio? Nostrum, est?</p>
 
       </section>
-      <section className='app__block'>
+      <section className='app__block_splitted'>
         <p className='app__paragraph app__paragraph_underlined'>        Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Non ullam ad debitis sint praesentium eveniet beatae corrupti itaque quae laudantium doloribus,
           aperiam libero voluptatem hic quod vero optio? Nostrum, est?</p>
 
+        <p className='app__paragraph'>Lorem ipsum dolor sit amet,consectetur
+          adipisicing elit. Suscipit cupiditate asperiores animi,
+          officia quo excepturi saepe provident reiciendis natus
+          alias a corrupti! Pariatur molestias ab quae odio ipsa
+          accusantium aliquid!</p>
       </section>
+
     </div>
   );
 }
